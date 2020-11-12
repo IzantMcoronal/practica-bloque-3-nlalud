88846eab0ed66f5d01e5dbf4d0e124d2fd1ddf36
IzantMcoronal <izant2020@gmail.com>
2020-11-12 15:11:35 +0100

    1 file modified

--- index.ts
+++ index.ts
@@ -5,11 +5,16 @@
 // OPCIONAL: Dentro de "getData" añadid un condicional con una guarda para diferenciar cuando reciba un tipo "number" y sacar por consola mensajes distintos para las dos llamadas anteriormente mencionadas.
 // Ánimo que ya lo tenéis! 
 
+//Importamos la interfaz dandole la ruta
 import {UserData} from "./user-data";
+
+//Creamos la variable donde almacenaremos los datos.
 let user : UserData;
 
+//Realizamos la creacion de la funcion generica que recibira dos tipos de datos, una de tipo numerica y la otra de tipo string.
 function getData <T> (arg: T): void{
   
+  //Condicional para el salvaguarda donde nos dara los dos mensajes. el primero nos sacara el numerod e telefono y el segundo nos dara el nombre.
   if (isNumber (arg)){
     console.log("Mensaje1= Phone: " + arg)
 
@@ -17,15 +22,23 @@ function getData <T> (arg: T): void{
     console.log("Mensaje2= Name: " + arg)
   }
 }
+
+// Construimos el objeto, le damos el nombre, la edad y el telefono
 user = {
   name: "Juancho",
   age: 22,
   phone: 123412340
 };
 
+//Variable que almacenara el nombre del objeto
 let name = getData <String> (user.name);
+
+//Varible que almacenara el numero de telefono del objeto.
 let phone = getData <number> (user.phone);
 
+//Funcion salvaguarda que comprueba si es un numero de tipo booleana 
 function isNumber<T>(arg: T): boolean {
+  
+  //Nos devuelve el resultado de la igualacion de si es o no un numero.
   return typeof arg === "number";
 }
